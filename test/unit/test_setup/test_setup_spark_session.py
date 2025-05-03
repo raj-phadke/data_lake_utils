c96e5c425641bac74c63768c1fe3a19d43814024
Raj Phadke <phadke.raj10@gmail.com>
2025-05-03 17:03:10 -0400

    adding spark session creation

---
+++ test/unit/test_setup/test_setup_spark_session.py
@@ -0,0 +1,26 @@
+def test_basic_spark_session(basic_spark_session):
+    spark = basic_spark_session
+    assert spark.sparkContext.appName == "TestAppBasic"
+    assert spark.sparkContext.getConf().get("spark.master") == "local[*]"
+
+
+def test_iceberg_spark_session(iceberg_spark_session):
+    spark = iceberg_spark_session
+    conf = spark.sparkContext.getConf()
+    assert (
+        conf.get("spark.jars.packages")
+        == "org.apache.iceberg:iceberg-spark-runtime-3.3_2.12:1.4.2"
+    )
+    assert (
+        conf.get("spark.sql.catalog.test_catalog")
+        == "org.apache.iceberg.spark.SparkCatalog"
+    )
+    assert conf.get("spark.sql.catalog.test_catalog.type") == "hadoop"
+    assert (
+        conf.get("spark.sql.catalog.test_catalog.warehouse")
+        == "file:///tmp/iceberg-warehouse"
+    )
+    assert (
+        conf.get("spark.sql.extensions")
+        == "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions"
+    )
